0abd4e0d575f49cc92bbf87a1f28bc70041c5e14
Pinaki Sarkar <pinaki.sarkar@calculi.com>
2022-06-22 21:44:26 -0700

    CAT-304 Implementation of azure tasks for provisioning network security groups

--- upup/pkg/fi/cloudup/azuretasks/networksecuritygroup.go
+++ upup/pkg/fi/cloudup/azuretasks/networksecuritygroup.go
@@ -16,7 +16,15 @@ limitations under the License.
 
 package azuretasks
 
-import "k8s.io/kops/upup/pkg/fi"
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-06-01/network"
+	"github.com/Azure/go-autorest/autorest/to"
+	"k8s.io/klog"
+	"k8s.io/kops/upup/pkg/fi"
+	"k8s.io/kops/upup/pkg/fi/cloudup/azure"
+)
 
 type NetworkSecurityGroup struct {
 	Name      *string
@@ -34,6 +42,79 @@ type NetworkSecurityGroup struct {
 	Tags map[string]*string
 }
 
-func (e *NetworkSecurityGroup) Run(c *fi.Context) error {
-	return fi.DefaultDeltaRunMethod(e, c)
+// CompareWithID returns the Name of the Network security group
+func (nsg *NetworkSecurityGroup) CompareWithID() *string {
+	return nsg.Name
+}
+
+// Find discovers the Network security group in the cloud provider
+func (nsg *NetworkSecurityGroup) Find(c *fi.Context) (*NetworkSecurityGroup, error) {
+	cloud := c.Cloud.(azure.AzureCloud)
+	l, err := cloud.NetworkSecurityGroup().List(context.TODO(), *nsg.ResourceGroup.Name)
+	if err != nil {
+		return nil, err
+	}
+	var found *network.SecurityGroup
+	for _, v := range l {
+		if *v.Name == *nsg.Name {
+			found = &v
+			klog.V(2).Infof("found matching Network security group %q", *found.ID)
+			break
+		}
+	}
+	if found == nil {
+		return nil, nil
+	}
+
+	return &NetworkSecurityGroup{
+		Name: nsg.Name,
+		Lifecycle: nsg.Lifecycle,
+		ResourceGroup: &ResourceGroup{
+			Name: nsg.ResourceGroup.Name,
+		},
+		Tags:     found.Tags,
+	}, nil
+}
+
+func (nsg *NetworkSecurityGroup) Run(c *fi.Context) error {
+	c.Cloud.(azure.AzureCloud).AddClusterTags(nsg.Tags)
+	return fi.DefaultDeltaRunMethod(nsg, c)
+}
+
+// CheckChanges returns an error if a change is not allowed.
+func (*NetworkSecurityGroup) CheckChanges(a, e, changes *NetworkSecurityGroup) error {
+	if a == nil {
+		// Check if required fields are set when a new resource is created.
+		if e.Name == nil {
+			return fi.RequiredField("Name")
+		}
+		return nil
+	}
+
+	// Check if unchanegable fields won't be changed.
+	if changes.Name != nil {
+		return fi.CannotChangeField("Name")
+	}
+	return nil
+}
+
+// RenderAzure creates or updates an Network security group.
+func (*NetworkSecurityGroup) RenderAzure(t *azure.AzureAPITarget, a, e, changes *NetworkSecurityGroup) error {
+	if a == nil {
+		klog.Infof("Creating a new Network security group with name: %s", fi.StringValue(e.Name))
+	} else {
+		klog.Infof("Updating an Network security group with name: %s", fi.StringValue(e.Name))
+	}
+	
+	nsg := network.SecurityGroup{
+		Name: to.StringPtr(*e.Name),
+		Location: to.StringPtr(t.Cloud.Region()),
+		Tags: e.Tags,
+	}
+
+	return t.Cloud.NetworkSecurityGroup().CreateOrUpdate(
+		context.TODO(),
+		*e.ResourceGroup.Name,
+		*e.Name,
+		nsg)
 }

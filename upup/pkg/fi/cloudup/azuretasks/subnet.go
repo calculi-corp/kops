cb6affd6bc056ea9bdcacee9c10128c286b3fa14
Pinaki Sarkar <pinaki.sarkar@calculi.com>
2022-07-30 19:38:13 -0700

    CAT-304 Remove condition which skips updating existing Subnet - we need to update it for the cluster to be functional

--- upup/pkg/fi/cloudup/azuretasks/subnet.go
+++ upup/pkg/fi/cloudup/azuretasks/subnet.go
@@ -132,11 +132,6 @@ func (*Subnet) CheckChanges(a, e, changes *Subnet) error {
 
 // RenderAzure creates or updates a subnet.
 func (*Subnet) RenderAzure(t *azure.AzureAPITarget, a, e, changes *Subnet) error {
-	if e.Shared != nil && *e.Shared {
-		klog.Infof("Shared subnet: %s - skipping create or update", fi.StringValue(e.Name))
-		return nil
-	}
-
 	if a == nil {
 		klog.Infof("Creating a new Subnet with name: %s", fi.StringValue(e.Name))
 	} else {
